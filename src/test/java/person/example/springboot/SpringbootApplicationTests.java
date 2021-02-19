c3bc02fb02f3c8dd53794cee993e204eeab4df8d
卢海鹏 <944068711@qq.com>
2021-02-19 10:29:46 +0800

    springboot启动研究程序

---
+++ src/test/java/person/example/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package person.example.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
